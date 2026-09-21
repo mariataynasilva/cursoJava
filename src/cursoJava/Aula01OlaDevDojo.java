95de60cbb6879a3b1501c4b25043fb627fc7fd2b
Maria tayna <mariataynagamer12@gmail.com>
2026-09-21 20:56:40 -0300

    commit iniciado

---
+++ src/cursoJava/Aula01OlaDevDojo.java
@@ -0,0 +1,14 @@
+package cursoJava;
+
+public class Aula01OlaDevDojo {
+    /**
+    * este é um comentario <b>javaDoc</b>
+    */
+
+    // isso é um comentario de 1 linha
+    /* isso é um comentario para multiplas linhas */
+
+    static void main(String[] args) {
+        System.out.println("ola mundo");
+    }
+}
---
+++ src/cursoJava/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package cursoJava;
+
+public class Aula02TiposPrimitivos {
+    static void main(String[] args) {
+        // int, double , float, char, byte, short, long, boolean
+
+    }
+
+}
+
+
+
