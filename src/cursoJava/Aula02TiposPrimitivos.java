65df7848f9ad7a5e86d6289cb6c2126295de8335
Maria tayna <mariataynagamer12@gmail.com>
2026-09-21 21:06:18 -0300

    commit iniciado

--- src/cursoJava/Aula02TiposPrimitivos.java
+++ src/cursoJava/Aula02TiposPrimitivos.java
@@ -4,7 +4,7 @@ public class Aula02TiposPrimitivos {
     static void main(String[] args) {
         // int, double , float, char, byte, short, long, boolean
          int idade = 10;
-        System.out.println(idade);
+        System.out.println("idade é "+idade+" anos");
     }
 
 }
