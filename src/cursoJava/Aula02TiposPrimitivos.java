c0d0756597de200a691450011a82db31f02e3c2d
Maria tayna <mariataynagamer12@gmail.com>
2026-09-22 07:31:06 -0300

    commit iniciado

--- src/cursoJava/Aula02TiposPrimitivos.java
+++ src/cursoJava/Aula02TiposPrimitivos.java
@@ -6,6 +6,7 @@ public class Aula02TiposPrimitivos {
 
          int idade =(int) 10000000000L; //isso aqui e um casting, ele vai fica cortando o numero, quando e numero grande
         // e pode muda a numeraçao por esta forçando a mudança de variavel, e melhor muda o tipo do que fazer isso.
+         long numeroGrande = (long) 155.23;
          double salarioDouble = 2000.0D;
          // convençao de codigo
          float salarioFloat = 2500.0F;
@@ -14,10 +15,14 @@ public class Aula02TiposPrimitivos {
          boolean verdadeiro = true;
          boolean falso = false;
          char caractere = '\u0043';
+         String nome = "alya";
+
 
          System.out.println("idade é "+idade+" anos");
-        System.out.println(verdadeiro);
-        System.out.println("char "+caractere);
+         System.out.println(verdadeiro);
+         System.out.println("char "+caractere);
+         System.out.println(numeroGrande);
+         System.out.println("seu nome é "+nome);
     }
 
 }
