67b33a4e67512a2123558e4f74fae23f3e80d98f
Maria tayna <mariataynagamer12@gmail.com>
2026-09-22 07:18:59 -0300

    commit iniciado

--- src/cursoJava/Aula02TiposPrimitivos.java
+++ src/cursoJava/Aula02TiposPrimitivos.java
@@ -3,10 +3,12 @@ package cursoJava;
 public class Aula02TiposPrimitivos {
     static void main(String[] args) {
         // int, double , float, char, byte, short, long, boolean
-         int idade = 10;
-         double salarioDouble = 2000;
+
+         int idade =(int) 10000000000L; //isso aqui e um casting, ele vai fica cortando o numero, quando e numero grande
+        // nao da pra fazer isso, nao é bom fazer, e melhor muda a variavel.
+         double salarioDouble = 2000.0D;
          // convençao de codigo
-         float salarioFloat = 2500;
+         float salarioFloat = 2500.0F;
          byte idadeByte = 10;
          short idadeShort = 10;
          boolean verdadeiro = true;
