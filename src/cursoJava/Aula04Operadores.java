0d4048a9a7d568ec9f687e646e8a15f7fb8c33af
Maria tayna <mariataynagamer12@gmail.com>
2026-09-22 08:12:30 -0300

    commit iniciado

---
+++ src/cursoJava/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package cursoJava;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero01 + numero02;
+        System.out.println(resultado);
+    }
+}
