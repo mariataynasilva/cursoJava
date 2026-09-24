a386559631ba399f945d1ce207eb6814025aa757
Maria tayna <mariataynagamer12@gmail.com>
2026-09-23 22:36:47 -0300

    commit iniciado

--- src/cursoJava/Aula05EstruturaCondicionais.java
+++ src/cursoJava/Aula05EstruturaCondicionais.java
@@ -3,6 +3,18 @@ package cursoJava;
 public class Aula05EstruturaCondicionais {
     public static void main(String[] args) {
         // IF
-
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        if (isAutorizadoComprarBebida){
+            System.out.println("Autorizado a comprar bebida alcolica");
+        }
+        else{
+            System.out.println("nao autorizado a comprar bebida alcolica");
+        }
+        // nunca deve ser feito isso
+        boolean c = true;
+        if (c = false){
+            System.out.println("dentro de algo que nunca deve ser feito");
+        }
     }
 }
