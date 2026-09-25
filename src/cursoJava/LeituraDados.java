62f7150dcd82146ac482d3182e4e4f81b863afde
Maria tayna <mariataynagamer12@gmail.com>
2026-09-24 21:46:15 -0300

    testando o codigo sozinha, esse foi o codigo que eu fiz treinando um pouco

---
+++ src/cursoJava/LeituraDados.java
@@ -0,0 +1,22 @@
+package cursoJava;
+import java.util.Scanner;
+
+public class LeituraDados {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("digite seu salario:");
+        double salario = scanner.nextDouble();
+
+        System.out.println("ola ,"+nome+" seu salario é "+salario);
+
+        scanner.close();
+    }
+} ta bonitinho o codigo desse jeito ? a identaçao ta feia?
+
+
+
